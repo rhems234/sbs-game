4ee79f599f9116511baf8c7355e7ad1b66dfe318
rhems234 <rhems234@gmail.com>
2025-08-04 19:46:48 +0900

    (2025.8.4) 메모리 영역

---
+++ Class10th (Function)/Source.c
@@ -0,0 +1,183 @@
+﻿#include <stdio.h>
+#include <stdarg.h>
+
+void initialize()
+{
+
+	printf("initialize...\n");
+
+}
+
+float get_temperature() {
+	return 36.5f;
+}
+
+void update(int x, int y) {
+
+	printf("x = %d, y = %d\n", x, y);
+}
+
+void print_status(char grade, int height, float temperature) {
+
+	printf("등급 : %c\n", grade);
+	printf("키 : %d\n", height);
+	printf("체온 : %.2f\n", temperature);
+
+}
+
+void swap(int* a, int* b) {
+	int temp = 0;
+
+	temp = *a;
+	*a = *b;
+	*b = temp;
+
+	//printf("swap 후의 x1 : %d\nswap 후의 y1 : %d\n", x1, y1);
+}
+
+void find(int count) {
+
+	if (count == 0) {
+		return;
+	}
+
+	printf("현재 count의 값 : %d\n", count);
+
+	find(count - 1);
+}
+
+inline int square(int x) {
+	return x * x;
+}
+
+double average(int count, ...) {
+
+	// va_list : 가변 인수의 메모리 주소를 저장하는 포인터 변수입니다.
+
+	// va_start : 가변 인수를 가져올 수 있도록 포인터를 설정하는 함수 입니다.)
+
+	// va_arg : 가변 인수 포인터에서 특정 자료형의 크기만큼 값을 가져오는 함수 입니다.
+
+	// va_end : 가변 인수가 끝났을 때 포인터를 NULL로 초기화하는 함수 입니다.
+
+	va_list list;
+	va_start(list, count);
+
+	int sum = 0;
+
+	for (int i = 0; i < count; i++) {
+		int num = va_arg(list, int);
+		sum += num;
+	}
+
+	va_end(list);
+
+	return (double)sum / count;
+}
+
+void main()
+{
+#pragma region 네이밍 컨벤션
+
+	// 1. 카멜 표기법 - 첫 번째 단어를 소문자로 시작
+	//					그 다음 있는 단어들은 대문자로 시작
+	// 2. 파스칼 표기법 (함수, 클래스) - 첫번째 단어를 대문자로 시작
+	//
+	// 3. 스네이크 표기법 - 소문자 + 언더스코어
+
+#pragma endregion
+
+#pragma region 함수
+	// 하나의 특별한 목적의 작업을 수행하기 위해 
+	// 독립적으로 설계된 코드의 집합입니다.
+	//initialize();
+
+#pragma endregion
+
+#pragma region 반환형
+	// 함수가 실행을 끝내고 돌려주는 결과의 자료형입니다.
+
+	// 함수의 경우 자료형과 반환하는 값의 형태가 일치하지 않으면
+	// 원하는 값을 얻을 수 없습니다.
+
+	// printf("온도 : %.2f\n", get_temperature());
+
+#pragma endregion
+
+#pragma region 매개 변수
+	// 함수의 정의에서 전달받은 인수를 함수 내부로 전달하기 위해 사용하는 변수입니다.
+	/*int num = 33;
+	int num2 = 11;
+
+	update(num, num2);*/
+
+	// 매개 변수는 함수 내부에서만 연산이 이루어지며,
+	// 함수가 종료되면 메모리에서 해제됩니다.
+
+	// 하나의 함수에 서로 다른 자료형의 매개 변수를 함께
+	// 정의할 수 있으며, 여러 개의 매개 변수를 생성할 수 있습니다.
+
+	//print_status('A', 170, 36.5f);
+#pragma endregion
+
+#pragma region 인수
+	// 함수가 호출될 때 매개 변수에 실제로 전달되는 값입니다.
+
+	/*int a = 10;
+	int b = 20;
+
+	int temp = 0;
+
+	temp = a;
+	a = b;
+	b = temp;
+
+	printf("a : %d\n", a);
+	printf("b : %d\n", b);*/
+
+	//int x1 = 10;
+	//int y1 = 20;
+	//
+	//// swap 함수 내부에서 실행 후 출력
+	////swap(x1, y1);
+
+	//// 함수 외부에서 실행 후 출력을 위해 주소값 전달 
+	//swap(&x1, &y1);
+
+	//printf("swap 후의 x1 : %d\nswap 후의 y1 : %d\n", x1, y1);
+
+	// 인수의 경우 함수에 있는 매개 변수에 따라 전달할 수
+	// 있는 인수의 수가 결정되며, 값을 전달하는 인수와 값을
+	// 전달 받는 매개 변수의 자료형이 서로 일치해야 합니다.
+#pragma endregion
+
+#pragma region 재귀함수
+	// 어떤 함수에서 자신을 다시 호출하여 작업을
+	// 수행하는 함수 입니다.
+
+	// 재귀 함수는 함수를 계속 호출하기 때문에 스택 영역에
+	// 메모리가 게속 쌓이게 되므로 스택 오버플로우가 발생합니다.
+	//find(3);
+#pragma endregion
+
+#pragma region 인라인 함수
+	// 함수를 호출하는 대신 함수가 호출되는 위치마다 
+	// 함수의 코드를 복사하여 전달하는 방식의 함수입니다.
+
+	//printf("제곱의 값 : %d\n", square(9));
+
+	// 인라인 함수는 함수를 호출하는 과정이 없으므로 처리 속도가
+	// 빠르지만, 인라인 함수를 많이 사용하게 되면 함수의 코드가
+	// 복사되기 때문에 실행 파일에 크기가 커지게 됩니다.
+
+#pragma endregion
+
+#pragma region 가변 인수 목록
+	// 고정되어 있는 매개 변수 외에 개수가 정해지지 않은
+	// 인수를 추가로 받을 수 있는 인수입니다.
+
+	// printf("평균 : %.2f", average(4, 80, 90, 75, 60));	
+
+#pragma endregion
+
+}
--- Program/Source.c
+++ Program/Source.c
@@ -1,133 +1,39 @@
 ﻿#include <stdio.h>
 
-void initialize()
+int main()
 {
+#pragma region 메모리 영역
 
-	printf("initialize...\n");
+#pragma region Code 영역
+	// 프로그램을 실행하기 위해 필요한 코드가 저장되는 영역으로
+	// 함수의 주소와 상수가 저장되는 메모리 영역입니다.
+#pragma endregion
 
-}
+#pragma region Data 영역
+	// 프로그램의 시작과 함께 메모리에 저장되며, 프로그램이
+	// 종료될 때 메모리에서 해제되는 영역입니다.
+#pragma endregion
 
-float get_temperature() {
-	return 36.5f;
-}
+#pragma region BSS 영역
+	// 프로그램이 실행 될 때 초기화가 이루어지지 않은 전역 변수와
+	// 정적 변수가 저장되는 메모리 영역입니다.
+#pragma endregion
 
-void update(int x, int y) {
-	
-	printf("x = %d, y = %d\n", x, y);
-}
+#pragma region Stack 영역
+	// 프로그램이 자동으로 사용하는 임시 메모리 영역으로
+	// 함수 호출 시 생성되는 지역 변수와 매개 변수가 저장되는
+	// 메모리 영역 입니다.
+#pragma endregion
 
-void print_status(char grade, int height, float temperature) {
+#pragma region Heap 영역
+	// 사용자가 직접 메모리 공간을 할당해주는 메모리 영역이며,
+	// 사용하지 않는 경우 사용자가 직접 메모리를 해제해야 되는
+	// 메모리 영역입니다.
+#pragma endregion
 
-	printf("등급 : %c\n", grade);
-	printf("키 : %d\n", height);
-	printf("체온 : %.2f\n", temperature);
-
-}
-
-void swap(int *a, int *b) {
-	int temp = 0;
-
-	temp = *a;
-	*a = *b;
-	*b = temp;
-
-	//printf("swap 후의 x1 : %d\nswap 후의 y1 : %d\n", x1, y1);
-}
-
-void find(int count) {
-
-	if (count == 0) {
-		return;
-	}
-
-	printf("현재 count의 값 : %d\n", count);
-
-	find(count - 1);
-}
-
-void main()
-{
-#pragma region 네이밍 컨벤션
-
-	// 1. 카멜 표기법 - 첫 번째 단어를 소문자로 시작
-	//					그 다음 있는 단어들은 대문자로 시작
-	// 2. 파스칼 표기법 (함수, 클래스) - 첫번째 단어를 대문자로 시작
-	//
-	// 3. 스네이크 표기법 - 소문자 + 언더스코어
 
 #pragma endregion
 
-#pragma region 함수
-	// 하나의 특별한 목적의 작업을 수행하기 위해 
-	// 독립적으로 설계된 코드의 집합입니다.
-	//initialize();
 
-#pragma endregion
-
-#pragma region 반환형
-	// 함수가 실행을 끝내고 돌려주는 결과의 자료형입니다.
-
-	// 함수의 경우 자료형과 반환하는 값의 형태가 일치하지 않으면
-	// 원하는 값을 얻을 수 없습니다.
-
-	// printf("온도 : %.2f\n", get_temperature());
-
-#pragma endregion
-
-#pragma region 매개 변수
-	// 함수의 정의에서 전달받은 인수를 함수 내부로 전달하기 위해 사용하는 변수입니다.
-	/*int num = 33;
-	int num2 = 11;
-
-	update(num, num2);*/
-
-	// 매개 변수는 함수 내부에서만 연산이 이루어지며,
-	// 함수가 종료되면 메모리에서 해제됩니다.
-
-	// 하나의 함수에 서로 다른 자료형의 매개 변수를 함께
-	// 정의할 수 있으며, 여러 개의 매개 변수를 생성할 수 있습니다.
-
-	//print_status('A', 170, 36.5f);
-#pragma endregion
-
-#pragma region 인수
-	// 함수가 호출될 때 매개 변수에 실제로 전달되는 값입니다.
-
-	/*int a = 10;
-	int b = 20;
-
-	int temp = 0;
-
-	temp = a;
-	a = b;
-	b = temp;
-
-	printf("a : %d\n", a);
-	printf("b : %d\n", b);*/
-
-	//int x1 = 10;
-	//int y1 = 20;
-	//
-	//// swap 함수 내부에서 실행 후 출력
-	////swap(x1, y1);
-
-	//// 함수 외부에서 실행 후 출력을 위해 주소값 전달 
-	//swap(&x1, &y1);
-
-	//printf("swap 후의 x1 : %d\nswap 후의 y1 : %d\n", x1, y1);
-
-	// 인수의 경우 함수에 있는 매개 변수에 따라 전달할 수
-	// 있는 인수의 수가 결정되며, 값을 전달하는 인수와 값을
-	// 전달 받는 매개 변수의 자료형이 서로 일치해야 합니다.
-#pragma endregion
-
-#pragma region 재귀함수
-	// 어떤 함수에서 자신을 다시 호출하여 작업을
-	// 수행하는 함수 입니다.
-
-	// 재귀 함수는 함수를 계속 호출하기 때문에 스택 영역에
-	// 메모리가 게속 쌓이게 되므로 스택 오버플로우가 발생합니다.
-	//find(3);
-#pragma endregion
-
+	return 0;
 }
